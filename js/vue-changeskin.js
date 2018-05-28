b5e178ba429c8f1b32214a069e6e61d9a19e198e
stage <1311012326@qq.com>
2018-05-28 09:25:20 +0800

    微调样式

--- js/vue-changeskin.js
+++ js/vue-changeskin.js
@@ -4,7 +4,7 @@ var vueChangeskin = {
         <span class="default" @click="$emit('changeskin','default')"></span>
         <span class="pink" @click="$emit('changeskin','pink')"></span>
         <span class="blue" @click="$emit('changeskin','blue')"></span>
-        <span class="purple" @click="$emit('changeskin','purple')"></span>
+        <span class="purple" @click="$emit('changeskin','gray')"></span>
         </div>
     </div>`,
 };
